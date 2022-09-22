912d17ad5cb7cd8577d97f7d226868d747effef0
Ahmed Khaled <70321297+ahmedkhaled24@users.noreply.github.com>
2022-09-22 20:35:00 +0200

    complete splash activity

---
+++ app/src/main/java/com/example/storeapp/view/ui/activites/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.storeapp.view.ui.activites
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import com.example.storeapp.MainActivity
+import com.example.storeapp.R
+import com.example.storeapp.view.BaseComponents.BaseActivity
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Execute a task in the background thread after 3 seconds.
+        openMainActivity()
+    }
+
+    private fun openMainActivity() {
+        Executors.newSingleThreadScheduledExecutor().schedule({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3, TimeUnit.SECONDS)
+    }
+}
